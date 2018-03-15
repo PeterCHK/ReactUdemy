4c1e7f5d867c0c9362b55cd4e46f0d3f58e2307f
DayOneMile <peter@peterchong.org>
2018-03-15 11:00:21 +0800

    finished lecture31

--- src/index.js
+++ src/index.js
@@ -26,11 +26,11 @@ class App extends Component {
       videos: [],
       selectedVideo: null
     };
-    this.videoSearch('surfboards');
+    this.videoSearch('cute puppy');
   }
 
   videoSearch(term) {
-    YTSearch({key: API_KEY, term: 'term'}, videos => {
+    YTSearch({key: API_KEY, term: term}, videos => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -41,7 +41,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar onSearchTermChange={term => this.videoSearch(term)}  />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
